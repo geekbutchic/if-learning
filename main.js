70720d3536c515c18cbe29334926a9276b7a4d7d
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-09-28 01:26:54 -0400

    completed if-learning functions

--- main.js
+++ main.js
@@ -3,9 +3,46 @@
  ******************************/
 
 
+ function isItLong (str) {
+  if (str.length > 20) {
+    return "That's a long string!"
+  } 
+ }
 
+function isItMedium (str) {
+  if (str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!"
+  }
+}
 
+function isItShort (str) {
+  if (str.length < 10) {
+    return "That's a small string!"
+  } else {
+    return "That's not a small string!"
+  }
+}
 
+function howLongIsMyString (str) {
+  if (str.length > 20) {
+    return "That's a long string!"
+  } if (str.length >= 10 && str.length <= 20) {
+    return "That's a regular sized string!"
+  } if (str.length < 10) {
+    return "That's a small string!"
+  }
+}
+
+function instructorHeight (name) {
+  if (name === "Colin") {
+    return 62
+  } if (name === "Mesuara") {
+    return 69
+  } if ("!Colin" || "!Mesuara")
+    return "I don't know that instructor!"
+  }
+
+  
 
 /**********************************
  *      OUR CODE GOES BELOW.      *
